ba879e023eea24f5329266b1b2790713494b422d
23770247 <83153449+23770247@users.noreply.github.com>
2021-05-01 22:03:36 -0700

    Add files via upload

---
+++ pre.java
@@ -0,0 +1,22 @@
+/**
+ * Class Prey represents any prey object.
+ *
+ * @author 
+ * @version
+ */
+//this is our pray class 
+
+// abstract class for pray 
+public class Prey {
+    /**
+     * Constructor for objects of class Prey
+     */
+
+//this is our constructor class
+    public Prey()
+    {
+        // initialise instance variables        
+
+
+    }    
+}
